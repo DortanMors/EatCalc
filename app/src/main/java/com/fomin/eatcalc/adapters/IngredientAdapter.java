e8f20f23e45a7e981b9979a66e8b163edda16482
DortanMors <dortan_mors@mail.ru>
2020-10-04 16:36:00 +0400

    ingredient adapter implement

---
+++ app/src/main/java/com/fomin/eatcalc/adapters/IngredientAdapter.java
@@ -0,0 +1,62 @@
+package com.fomin.eatcalc.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.fomin.eatcalc.R;
+import com.fomin.eatcalc.datastorage.Ingredient;
+
+import java.util.List;
+
+class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.ViewHolder> {
+   private LayoutInflater inflater;
+   private List<Ingredient> ingredients;
+
+   IngredientAdapter(Context context, List<Ingredient> ingredients){
+      this.inflater = LayoutInflater.from(context);
+      this.ingredients = ingredients;
+   }
+
+   @NonNull
+   @Override
+   public IngredientAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      View view = inflater.inflate(R.layout.prices_list_item, parent, false);
+      return new ViewHolder(view);
+   }
+
+   @Override
+   public void onBindViewHolder(@NonNull IngredientAdapter.ViewHolder holder, int position) {
+      Ingredient ingredient = ingredients.get(position);
+      holder.countView.setText(String.valueOf(1)); // price is always for 1 unit
+      holder.unitView.setText(ingredient.getUnit());
+      holder.nameView.setText(ingredient.getName());
+      holder.priceView.setText(String.valueOf(ingredient.getPrice()));
+      holder.currencyView.setText(ingredient.getCurrency());
+   }
+
+   @Override
+   public int getItemCount() {
+      return ingredients.size();
+   }
+
+
+   public class ViewHolder extends RecyclerView.ViewHolder{
+
+      final TextView countView, unitView, nameView, priceView, currencyView;
+
+      public ViewHolder(@NonNull View itemView) {
+         super(itemView);
+         countView    = (TextView) itemView.findViewById(R.id.item_count);
+         unitView     = (TextView) itemView.findViewById(R.id.item_unit);
+         nameView     = (TextView) itemView.findViewById(R.id.item_name);
+         priceView    = (TextView) itemView.findViewById(R.id.item_price);
+         currencyView = (TextView) itemView.findViewById(R.id.item_currency);
+      }
+   }
+}
--- app/src/main/java/com/fomin/eatcalc/datastorage/Ingredient.java
+++ app/src/main/java/com/fomin/eatcalc/datastorage/Ingredient.java
@@ -13,5 +13,24 @@ public class Ingredient {
 
     public double price;
 
-    public double unit_id;
+    public long unit_id;
+
+    public long currency_id;
+
+
+    public String getCurrency() { // TODO: механизм получения валюты из id в string
+        return String.valueOf(currency_id);
+    }
+
+    public String getUnit() { // TODO: механизм получения единицы измерения из id в string
+        return String.valueOf(unit_id);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
