03aafbbd00d1bef38e6b84e17bf6c6d343c4e906
DortanMors <dortan_mors@mail.ru>
2020-10-11 11:58:53 +0400

    Navigator

---
+++ app/src/main/java/com/fomin/eatcalc/activity/navigator/Navigator.java
@@ -0,0 +1,28 @@
+package com.fomin.eatcalc.activity.navigator;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import com.fomin.eatcalc.R;
+import com.fomin.eatcalc.activity.AddIngredientActivity;
+import com.fomin.eatcalc.activity.IngredientsListActivity;
+
+public class Navigator implements View.OnClickListener {
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+        Context contextActivity = view.getContext();
+        if(viewId== R.id.prices){
+            Intent intent = new Intent(contextActivity, IngredientsListActivity.class);
+            contextActivity.startActivity(intent);
+        } else if(viewId==R.id.add_ingredient_price){
+            Intent intent = new Intent(contextActivity, AddIngredientActivity.class);
+            contextActivity.startActivity(intent);
+        } else if(viewId == R.id.save_new_ingredient){
+            Intent intent = new Intent(contextActivity, IngredientsListActivity.class);
+            contextActivity.startActivity(intent);
+        }
+    }
+}
