cb4da611eb5cc34bcb72f8b7fce0b4cc6561fbc0
DortanMors <dortan_mors@mail.ru>
2020-10-17 20:48:27 +0400

    autogenerate id

--- app/src/main/java/com/fomin/eatcalc/datastorage/Ingredient.java
+++ app/src/main/java/com/fomin/eatcalc/datastorage/Ingredient.java
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey;
 @Entity
 public class Ingredient {
 
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     public long id;
 
     public String name;
--- app/src/main/java/com/fomin/eatcalc/datastorage/Recipe.java
+++ app/src/main/java/com/fomin/eatcalc/datastorage/Recipe.java
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey;
 @Entity
 public class Recipe {
 
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     public long id;
 
     public String name;
