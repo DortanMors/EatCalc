5b1457d3f25962a066534016dc4a438952f720e9
DortanMors <dortan_mors@mail.ru>
2020-10-17 20:28:51 +0400

    delete app class

--- app/src/main/java/com/fomin/eatcalc/activity/IngredientsListActivity.java
+++ app/src/main/java/com/fomin/eatcalc/activity/IngredientsListActivity.java
@@ -8,15 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.fomin.eatcalc.App;
 import com.fomin.eatcalc.R;
 import com.fomin.eatcalc.activity.navigator.Navigator;
 import com.fomin.eatcalc.adapters.IngredientAdapter;
 import com.fomin.eatcalc.datastorage.AppDatabase;
 import com.fomin.eatcalc.datastorage.Ingredient;
-import com.fomin.eatcalc.datastorage.IngredientDao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class IngredientsListActivity extends AppCompatActivity {
@@ -30,10 +27,7 @@ public class IngredientsListActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients_list);
-        db = App.getInstance().getDatabase();
-        IngredientDao ingredientDao = db.ingredientDao();
-        ingredients = ingredientDao.getAll();
-
+        
         ingredientsList = (RecyclerView) findViewById(R.id.ingredients_list);
         IngredientAdapter adapter = new IngredientAdapter(this, ingredients);
         ingredientsList.setAdapter(adapter);
