920a9f641f87fcea18fdf7124b05eecf9ec41943
DortanMors <dortan_mors@mail.ru>
2020-10-18 21:09:55 +0400

    add recipe activities

---
+++ app/src/main/java/com/fomin/eatcalc/activity/AddRecipeActivity.java
@@ -0,0 +1,6 @@
+package com.fomin.eatcalc.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddRecipeActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/fomin/eatcalc/activity/RecipesListActivity.java
@@ -0,0 +1,6 @@
+package com.fomin.eatcalc.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipesListActivity extends AppCompatActivity {
+}
