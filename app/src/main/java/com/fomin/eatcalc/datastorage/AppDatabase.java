96402f67ee2d1fcfac8d47465e76130c0973a2e6
DortanMors <dortan_mors@mail.ru>
2020-11-04 00:51:10 +0400

    type converters

--- app/src/main/java/com/fomin/eatcalc/datastorage/AppDatabase.java
+++ app/src/main/java/com/fomin/eatcalc/datastorage/AppDatabase.java
@@ -6,12 +6,14 @@ import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Database(entities = {Ingredient.class, Recipe.class, Unit.class}, version = 1)
+@TypeConverters({MapConverters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract IngredientDao ingredientDao();
     public abstract RecipeDao recipeDao();
