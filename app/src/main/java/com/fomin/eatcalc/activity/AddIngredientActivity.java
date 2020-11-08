ab899ed2b620a235bf07925f5d75d7d6ec2d1759
DortanMors <dortan_mors@mail.ru>
2020-11-08 17:27:53 +0400

    add ingredient validation improvements

--- app/src/main/java/com/fomin/eatcalc/activity/AddIngredientActivity.java
+++ app/src/main/java/com/fomin/eatcalc/activity/AddIngredientActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 
 import com.fomin.eatcalc.R;
@@ -23,6 +21,7 @@ public class AddIngredientActivity extends AppCompatActivity {
     private TextInputEditText nameEditText;
     private TextInputEditText priceEditText;
     private TextInputEditText currencyEditText;
+    private final Context context = this;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,52 +34,55 @@ public class AddIngredientActivity extends AppCompatActivity {
         currencyEditText = findViewById(R.id.add_ingredient_currency);
 
         final Button button_submit = findViewById(R.id.save_new_ingredient);
-        Context context = this;
-        button_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent replyIntent = new Intent();
-                isError = false;
-                double count;
-                try {
-                    count = Double.parseDouble(countEditText.getText().toString());
-                } catch (Exception e) {
-                    count = 0;
-                    isError = true;
-                    countEditText.setError(context.getString(R.string.input_number));
-                }
-                String units = unitsEditText.getText().toString();
-                String name = nameEditText.getText().toString();
-                double price;
-                try {
-                    price = Double.parseDouble(priceEditText.getText().toString());
-                } catch (Exception e) {
-                    price = 0;
-                    isError = true;
-                    priceEditText.setError(context.getString(R.string.input_number));
-                }
-                String currency = currencyEditText.getText().toString();
+        button_submit.setOnClickListener(v -> {
+            Intent replyIntent = new Intent();
+            isError = false;
 
-                ValidatorsComposer<String> validation = new ValidatorsComposer<>(new EmptyValidator(context));
-                if (!validation.isValid(units)) {
-                    unitsEditText.setError(validation.getMessage());
-                } else if(!validation.isValid(name)) {
-                    nameEditText.setError(validation.getMessage());
-                } else if(!validation.isValid(currency)) {
-                    currencyEditText.setError(validation.getMessage());
-                } else  { // TODO: добавить проверку ввода
+            double count;
+            double price;
+            String units = unitsEditText.getText().toString();
+            String name = nameEditText.getText().toString();
+            String currency = currencyEditText.getText().toString();
 
-                    // TODO: извлечь строки в переменные
-                    replyIntent.putExtra("count", count);
-                    replyIntent.putExtra("units", units);
-                    replyIntent.putExtra("name", name);
-                    replyIntent.putExtra("price", price);
-                    replyIntent.putExtra("currency", currency);
-                    // TODO: если все данные в порядке
-                    setResult(RESULT_OK, replyIntent);
-                    isValid = true;
-                    finish();
-                }
+            try {
+                count = Double.parseDouble(countEditText.getText().toString());
+            } catch (Exception e) {
+                count = 0;
+                isError = true;
+                countEditText.setError(context.getString(R.string.input_number));
+            }
+            try {
+                price = Double.parseDouble(priceEditText.getText().toString());
+            } catch (Exception e) {
+                price = 0;
+                isError = true;
+                priceEditText.setError(context.getString(R.string.input_number));
+            }
+
+            ValidatorsComposer<String> validation = new ValidatorsComposer<>(new EmptyValidator(context));
+            if (!validation.isValid(units)) {
+                unitsEditText.setError(validation.getMessage());
+                isError = true;
+            }
+            if(!validation.isValid(name)) {
+                nameEditText.setError(validation.getMessage());
+                isError = true;
+            }
+            if(!validation.isValid(currency)) {
+                currencyEditText.setError(validation.getMessage());
+                isError = true;
+            }
+            if(!isError) {
+                // TODO: извлечь строки в переменные
+                replyIntent.putExtra("count", count);
+                replyIntent.putExtra("units", units);
+                replyIntent.putExtra("name", name);
+                replyIntent.putExtra("price", price);
+                replyIntent.putExtra("currency", currency);
+
+                setResult(RESULT_OK, replyIntent);
+                isValid = true;
+                finish();
             }
         });
     }
