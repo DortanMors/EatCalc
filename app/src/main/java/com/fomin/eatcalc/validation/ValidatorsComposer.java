011afddbe2251bd64ca67e7a1048acd5443c526f
DortanMors <dortan_mors@mail.ru>
2020-11-08 14:57:40 +0400

    add constructor to ValidatorsComposer

--- app/src/main/java/com/fomin/eatcalc/validation/ValidatorsComposer.java
+++ app/src/main/java/com/fomin/eatcalc/validation/ValidatorsComposer.java
@@ -1,11 +1,16 @@
 package com.fomin.eatcalc.validation;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ValidatorsComposer<T> implements Validator<T> {
 
-    private List<Validator<T>> validators;
+    private final List<Validator<T>> validators;
     private String message;
+
+    public ValidatorsComposer(Validator<T>... validators) {
+        this.validators = Arrays.asList(validators);
+    }
 
     @Override
     public boolean isValid(T value) {
