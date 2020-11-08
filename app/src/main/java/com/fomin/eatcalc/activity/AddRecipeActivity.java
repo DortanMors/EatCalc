f640e1e04b133c75e7cacba9f37249c0e40c80de
DortanMors <dortan_mors@mail.ru>
2020-11-08 17:28:04 +0400

    add recipe improvements

--- app/src/main/java/com/fomin/eatcalc/activity/AddRecipeActivity.java
+++ app/src/main/java/com/fomin/eatcalc/activity/AddRecipeActivity.java
@@ -3,8 +3,6 @@ package com.fomin.eatcalc.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -12,8 +10,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.fomin.eatcalc.adapters.RecipeIngredientsAdapter;
 import com.fomin.eatcalc.datastorage.Ingredient;
+import com.fomin.eatcalc.validation.EmptyValidator;
+import com.fomin.eatcalc.validation.ValidatorsComposer;
 import com.fomin.eatcalc.viewmodels.IngredientViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
@@ -21,6 +24,8 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.fomin.eatcalc.R;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class AddRecipeActivity extends AppCompatActivity {
@@ -32,6 +37,9 @@ public class AddRecipeActivity extends AppCompatActivity {
     private HashMap<Long, Double> counts;
     private double mass = 0;
     private double price = 0;
+    private boolean isValid = false;
+    private boolean isError = false;
+    private final Context context = this;
     private List<Ingredient> ingredients = new LinkedList<>();
     private RecipeIngredientsAdapter adapter;
 
@@ -52,37 +60,57 @@ public class AddRecipeActivity extends AppCompatActivity {
         ingredientsView.setLayoutManager(new LinearLayoutManager(this));
 
         FloatingActionButton button_add_ingredient = findViewById(R.id.add_recipe_ingredient);
-        button_add_ingredient.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent editIngredients = new Intent(AddRecipeActivity.this, EditIngredientsActivity.class);
-                editIngredients.putExtra("counts", counts);
-                startActivityForResult(editIngredients, EDIT_RECIPE_INGREDIENTS_REQUEST_CODE);
-            }
+        button_add_ingredient.setOnClickListener(v -> {
+            Intent editIngredients = new Intent(AddRecipeActivity.this, EditIngredientsActivity.class);
+            editIngredients.putExtra("counts", counts);
+            startActivityForResult(editIngredients, EDIT_RECIPE_INGREDIENTS_REQUEST_CODE);
         });
 
         Button button_submit = findViewById(R.id.new_recipe_save);
-        button_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent addRecipe = new Intent();
-                if(TextUtils.isEmpty(name.getText())) {
-                    setResult(RESULT_CANCELED, addRecipe);
-                } else { // TODO: добавить валидацию всех инпутов
-                    int portionsCount = Integer.parseInt(portionsNum.getText().toString());
-                    String recipeName = name.getText().toString();
-                    String cookingMethod = method.getText().toString();
-                    // TODO: извлечь строки в переменные
-                    addRecipe.putExtra("portionsCount", portionsCount);
-                    addRecipe.putExtra("name", recipeName);
-                    addRecipe.putExtra("cookingMethod", cookingMethod);
+        button_submit.setOnClickListener(v -> {
+            Intent addRecipe = new Intent();
+            isError = false;
 
-                    addRecipe.putExtra("resultCounts", counts);
-                    addRecipe.putExtra("price", price);
-                    addRecipe.putExtra("mass", mass);
-                    // TODO: если все данные в порядке
-                    setResult(RESULT_OK, addRecipe);
-                }
+            int portionsCount;
+            try {
+                portionsCount = Integer.parseInt(portionsNum.getText().toString());
+            } catch (Exception e) {
+                portionsCount = 0;
+                portionsNum.setError(context.getString(R.string.input_number));
+                isError = true;
+            }
+            String recipeName = name.getText().toString();
+            String cookingMethod = method.getText().toString();
+
+            ValidatorsComposer<String> validation = new ValidatorsComposer<>(new EmptyValidator(context));
+            if(!validation.isValid(recipeName)) {
+                name.setError(validation.getMessage());
+                isError = true;
+            }
+            if(!validation.isValid(cookingMethod)) {
+                method.setError(validation.getMessage());
+                isError = true;
+            }
+            if(counts.isEmpty()) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        context.getString(R.string.recipe_has_no_ingredients),
+                        Toast.LENGTH_LONG
+                ).show();
+                isError= true;
+            }
+            if(!isError){
+                // TODO: извлечь строки в переменные
+                addRecipe.putExtra("portionsCount", portionsCount);
+                addRecipe.putExtra("name", recipeName);
+                addRecipe.putExtra("cookingMethod", cookingMethod);
+
+                addRecipe.putExtra("resultCounts", counts);
+                addRecipe.putExtra("price", price);
+                addRecipe.putExtra("mass", mass);
+
+                setResult(RESULT_OK, addRecipe);
+                isValid = true;
                 finish();
             }
         });
@@ -94,6 +122,16 @@ public class AddRecipeActivity extends AppCompatActivity {
 
         if((requestCode == EDIT_RECIPE_INGREDIENTS_REQUEST_CODE) && (resultCode == RESULT_OK)) {
             counts = (HashMap<Long, Double>) data.getSerializableExtra("resultCounts");
+
+            if(counts.isEmpty()) { // TODO: одинаковые тосты в функции
+                Toast.makeText(
+                        getApplicationContext(),
+                        context.getString(R.string.recipe_has_no_ingredients),
+                        Toast.LENGTH_LONG
+                ).show();
+                isError= true;
+            }
+
             price = 0;
             mass = 0;
 
@@ -121,13 +159,23 @@ public class AddRecipeActivity extends AppCompatActivity {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         } else {
-            Toast.makeText(
-                    getApplicationContext(),
-                    getString(R.string.recipe_not_saved),
-                    Toast.LENGTH_LONG
-            ).show();
+            if(counts.isEmpty()) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        context.getString(R.string.recipe_has_no_ingredients),
+                        Toast.LENGTH_LONG
+                ).show();
+                isError= true;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(!isValid) {
+            setResult(RESULT_CANCELED, new Intent());
         }
     }
 
